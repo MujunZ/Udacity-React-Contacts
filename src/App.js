9f42c726ea43aba6b38365f7d6d28765e938a7b7
MujunZ <o.6180339@gmail.com>
2017-07-08 22:53:43 -0400

    Add State

--- src/App.js
+++ src/App.js
@@ -1,32 +1,57 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts.js'
 
-const contacts = [
-  {
-    "id": "ryan",
-    "name": "Ryan Florence",
-    "email": "ryan@reacttraining.com",
-    "avatarURL": "http://localhost:5001/ryan.jpg"
-  },
-  {
-    "id": "michael",
-    "name": "Michael Jackson",
-    "email": "michael@reacttraining.com",
-    "avatarURL": "http://localhost:5001/michael.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "email": "tyler@reacttraining.com",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-];
+// const contacts = [
+//   {
+//     "id": "ryan",
+//     "name": "Ryan Florence",
+//     "email": "ryan@reacttraining.com",
+//     "avatarURL": "http://localhost:5001/ryan.jpg"
+//   },
+//   {
+//     "id": "michael",
+//     "name": "Michael Jackson",
+//     "email": "michael@reacttraining.com",
+//     "avatarURL": "http://localhost:5001/michael.jpg"
+//   },
+//   {
+//     "id": "tyler",
+//     "name": "Tyler McGinnis",
+//     "email": "tyler@reacttraining.com",
+//     "avatarURL": "http://localhost:5001/tyler.jpg"
+//   }
+// ];
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      contacts:[
+        {
+          "id": "ryan",
+          "name": "Ryan Florence",
+          "email": "ryan@reacttraining.com",
+          "avatarURL": "http://localhost:5001/ryan.jpg"
+        },
+        {
+          "id": "michael",
+          "name": "Michael Jackson",
+          "email": "michael@reacttraining.com",
+          "avatarURL": "http://localhost:5001/michael.jpg"
+        },
+        {
+          "id": "tyler",
+          "name": "Tyler McGinnis",
+          "email": "tyler@reacttraining.com",
+          "avatarURL": "http://localhost:5001/tyler.jpg"
+        }
+      ]
+    };
+  }
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts} />
+        <ListContacts contacts={this.state.contacts} />
       </div> 
     );
   }
