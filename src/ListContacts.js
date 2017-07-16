8957b1273c454ee6d649786fe80231de7bcf2bcf
MujunZ <o.6180339@gmail.com>
2017-07-15 20:45:33 -0400

    Add propTypes

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const ListContacts = (props) => 
-  (
-    <ol className='contact-list'>
-      {props.contacts.map((contact) => (
-        <li key={contact.id} className="contact-list-item">
-          <div className='contact-avatar' style={{backgroundImage: `url(${contact.avatarURL})`}}/>
-          <div className='contact-details'>
-            <div>{contact.name}</div>
-            <div>{contact.email}</div>
-          </div>
-        <button onClick={() => props.onDeleteContact(contact)} className='contact-remove'>
-            Remove
-        </button>
-    		</li>
-    		))}
-    </ol>
-  );
+class ListContacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
+  state = {
+    query: ''
+  }
+  render() {
+    return (
+        
+        <ol className='contact-list'>
+          {this.props.contacts.map((contact) => (
+            <li key={contact.id} className="contact-list-item">
+              <div className='contact-avatar' style={{backgroundImage: `url(${contact.avatarURL})`}}/>
+              <div className='contact-details'>
+                <div>{contact.name}</div>
+                <div>{contact.email}</div>
+              </div>
+            <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+                Remove
+            </button>
+            </li>
+            ))}
+        </ol>
+      )
+  }
+}
 
 export default ListContacts;
